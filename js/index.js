8cb47d3b32090eb31d29a3d52588dbd2879c4c92
sam-malanchuk <malanchuksam@gmail.com>
2019-05-02 21:58:42 -0400

    added 2 event listeners to index.js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,15 @@ siteHeading.addEventListener('mouseout', function(e) {
     e.target.style.textDecoration = 'none';
 });
 
+// Add a border to the image on click
+const headerImg = document.querySelector('.intro img');
+headerImg.addEventListener('click', function(e) {
+    e.target.style.border = "5px solid black";
+});
+
+//Double click anywhere on the body to change the background color
+const documentHome = document.querySelector('body');
+documentHome.addEventListener('dblclick', function(e) {
+    documentHome.style.backgroundColor = "#c2b280";
+});
+
