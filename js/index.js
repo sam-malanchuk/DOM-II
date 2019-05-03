e36b6fe8783bc753d02f1775d8a5176e9939ba79
sam-malanchuk <malanchuksam@gmail.com>
2019-05-02 21:40:10 -0400

    added 2 event listeners to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+// Mouse over the heading adds style, on mouse out sets to default
+const siteHeading = document.querySelector('.logo-heading');
+siteHeading.addEventListener('mouseover', function(e) {
+    e.target.style.color = 'red';
+    e.target.style.textDecoration = 'underline';
+});
+siteHeading.addEventListener('mouseout', function(e) {
+    e.target.style.color = 'black';
+    e.target.style.textDecoration = 'none';
+});
+
